dba4bc4d061bde622eab5972ca348d5a0f87c2c3
Raul <raul.lara.unam@gmail.com>
2023-03-30 16:01:28 -0600

    Update 1.0.1

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use( '/api/events', require('./routes/events'));
 //SOLUCION PARA NETLYFY
     app.use(express.static(path.join(__dirname, 'public')));
  
-    app.get('/*', function(req,res) {
+    app.get('*', function(req,res) {
             res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 
